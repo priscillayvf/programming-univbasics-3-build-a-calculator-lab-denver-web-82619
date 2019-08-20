75fabc11e4ffb55326acb2f288e847823f2e96f9
Priscilla Firpo <priscillayf@gmail.com>
2019-08-20 18:17:34 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,15 +3,15 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  p num2 - num1
+  p num1 - num2
 end
 
 def division(num1, num2)
-  p 
+  p num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  p num1 * num2
 end
 
 def modulo(num1, num2)
